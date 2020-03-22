ebc9a36fb9ed873b5c8412801d213aa92a573c11
unknown <lvova.nikol@mail.ru>
2020-03-22 14:38:31 +0300

    created console .net core project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _6Zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k;
+            Console.Write("k = ");
+            k = Convert.ToInt32(Console.ReadLine());
+            int c;
+            c = (k+7)%7;
+            Console.WriteLine(c);
+        }
+    }
+}
